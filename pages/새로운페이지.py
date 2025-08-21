6f758038652103c913498858e8f07d0a6c9df909
2530111 <2530111@sw.hs.kr>
2025-08-21 09:19:50 +0900

    Update 새로운페이지.py

--- pages/새로운페이지.py
+++ pages/새로운페이지.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-st.title("끼앙아아")
-st.write("안녕하세요")
-st.markdown("---") 
+st.title("Hello Streamlit 👋")
+st.write("이건 정말 간단한 예시야!")
+
+name = st.text_input("이름을 입력해봐")
+age = st.slider("나이", 0, 100, 20)
+
+if st.button("확인"):
+    st.success(f"안녕 {name}, 너의 나이는 {age}살이구나!")
+
